28d22700a2a17ce49db064c9c7b2887ab61ed7c4
yuriportfolio <37544475+yuriportfolio@users.noreply.github.com>
2019-12-21 18:23:45 +0200

    Update _siteConfig.js

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -29,16 +29,6 @@ export default {
     {
       name: 'Categories',
       link: '/categories'
-    },
-    {
-      name: 'Github Repo',
-      link: 'https://github.com/danielkellyio/awake-template',
-      target: '_blank'
-    },
-    {
-      name: 'Awake+ Premium',
-      link: 'https://awake-premium-lp.netlify.com/',
-      target: '_blank'
     }
   ],
   hero: {
@@ -53,13 +43,13 @@ export default {
 
   // Card Layout
   cards: {
-    imageDimensions: '2x3', // Options: 1x1, 5x4, 4x3, 3x2, 5x3, 16x9, 2x1, 3x1, 4x5, 3x4, 2x3, 3x5, 9x16
-    theme: 'grid' // Options: boxes, grid, image-grid
+    imageDimensions: '1x3', // Options: 1x1, 5x4, 4x3, 3x2, 5x3, 16x9, 2x1, 3x1, 4x5, 3x4, 2x3, 3x5, 9x16
+    theme: 'boxes' // Options: boxes, grid, image-grid
   },
 
   // Disqus
   disqus: {
-    on: true,
+    on: false,
     loadingStrategy: 'button', // Options: onload, lazy, button
     siteShortName: 'frent-car-rent' // 'blog-danielkelly-io'
   },
